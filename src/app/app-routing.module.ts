d1027d60efc0982fbe0fa7c7aa4bf0229818f9da
ToluwanimiO <toluwanimiosuolale@yahoo.co.uk>
2020-12-04 21:12:08 +0100

    Done with Sign In Interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"signIn",component:SignInComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
