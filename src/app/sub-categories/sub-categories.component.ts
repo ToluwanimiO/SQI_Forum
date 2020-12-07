e34fcb97bd888f542cfeaeea76afd6515cb42391
joshuaoyerinde <oyerindejoshua133@gamil.com>
2020-12-07 10:14:16 +0100

    done from jos

--- src/app/sub-categories/sub-categories.component.ts
+++ src/app/sub-categories/sub-categories.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sub-categories.component.css']
 })
 export class SubCategoriesComponent implements OnInit {
+  public topicsArray = [
 
+    {topic:'Lorem ipsum dolor sit aconsectetur adipisicing', categories:'hacks', upvote:'200',date:'19-4-2019',views:28},
+    {topic:'Lorem ipsum dolor sit aconsectetur adipisicing', categories:'software', upvote:'200',date:'19-4-2019',views:28},
+    {topic:'Lorem ipsum dolor sit aconsectetur adipisicing', categories:'soft', upvote:'200',date:'19-4-2019',views:28},
+    {topic:'Lorem ipsum dolor sit aconsectetur adipisicing', categories:'hacks', upvote:'200',date:'19-4-2019',views:28},
+    {topic:'Lorem ipsum dolor sit aconsectetur adipisicing', categories:'software', upvote:'200',date:'19-4-2019',views:28},
+    {topic:'Lorem ipsum dolor sit aconsectetur adipisicing', categories:'soft', upvote:'200',date:'19-4-2019',views:28},
+    ];
+public categories=['Tech','football','ICT','WebPrograming','Software','Tech','football','ICT','WebPrograming','Software','Tech','football','ICT','WebPrograming','Software']
   constructor() { }
 
   ngOnInit(): void {
