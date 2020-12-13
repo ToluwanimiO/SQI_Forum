b509b7a3f08ad4819f11df3345932c5f63a513a6
felixgit <felixadegboyega2019@gmail.com>
2020-12-13 14:29:54 +0100

    make changes on mostly design

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-categories',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 public isArrayCategories = ['Software','Hardware','Web development','Python','Django','Laptop','Computer','Digital-Maketing','Sofware','Hardware','Web','Python','Django','Laptop','Computer','Digital','Hardware','Web development','Python','Django'];
-  constructor() { }
+public categoriesArray;
+  constructor(public categories:CategoryService) { }
 
   ngOnInit(): void {
+    this.categories.getCategories().subscribe(data =>{
+      this.categoriesArray = data;
+      console.log(data);
+    })
   }
 
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -43,7 +43,7 @@ export class SignUpComponent implements OnInit {
     // console.log(this.userForm.controls['Email'])
     // let control = this.userForm.controls
     // let newUser ={email:control['Email'].value,username:control['Username'].value,password:control['Password'].value,c_password:control['C_Password'].value}
-    console.log(this.userForm.value)
+    // console.log(this.userForm.value)
     this.signUpService.addNewUser(this.userForm.value).subscribe((res:any)=>{
       console.log(res);
       this.router.navigate(["/signIn"])
