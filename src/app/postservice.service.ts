60cbe4f8400dce8d56e07659e157d6a3cb46edcb
ToluwanimiO <toluwanimiosuolale@gmail.com>
2022-12-29 14:02:26 +0100

    cors fix

--- src/app/postservice.service.ts
+++ src/app/postservice.service.ts
@@ -10,37 +10,55 @@ export class PostserviceService {
 
   constructor(public http: HttpClient, public AuthGuardService: AuthGuardService) { }
   public baseUrl = "https://sqiforum.herokuapp.com/api/v1";
+  public token = this.AuthGuardService.getToken();
+  public headers = new HttpHeaders({
+    'Access-Control-Allow-Origin': '*'
+  });
   // /api/v1/post/create/
   // http://sqiforum.herokuapp.com/api/v1/post/create/
   public getCategories() {
-    return this.http.get<any>(`${this.baseUrl}/categories/`,{observe:'response'})
+    return this.http.get<any>(`${this.baseUrl}/categories/`,{observe:'response',headers:this.headers},)
   }
   public postText(posttext) {
     // return this.http.post<any>(`${this.baseUrl}/post/create`, text)
-    return this.http.post("https://sqiforum.herokuapp.com/api/v1/post/create/", posttext)
+    return this.http.post("https://sqiforum.herokuapp.com/api/v1/post/create/", posttext,{headers:this.headers})
 
   }
   public retrieveLists(){
-    return this.http.get<any>(`${this.baseUrl}/post/list/`);
+    const token = this.AuthGuardService.getToken();
+    const headers = new HttpHeaders({
+      'Access-Control-Allow-Origin': '*'
+    });
+    return this.http.get<any>(`${this.baseUrl}/post/list/`, {headers:this.headers});
   }
   public retrieveListDetail(slug,id){
-    return this.http.get<any>(`${this.baseUrl}/post/${slug}/${id}`)
+    const headers = new HttpHeaders({
+      'Access-Control-Allow-Origin': '*'
+    });
+    return this.http.get<any>(`${this.baseUrl}/post/${slug}/${id}`, {headers:this.headers})
   }
   upvote(post_id){
-    return this.http.post<any>(`${this.baseUrl}/post/${post_id}/upvote/`,"")
+    const headers = new HttpHeaders({
+      'Access-Control-Allow-Origin': '*'
+    });
+    return this.http.post<any>(`${this.baseUrl}/post/${post_id}/upvote/`,"",{headers:this.headers})
   }
 
   public getPosts() {
       const token = this.AuthGuardService.getToken();
       const headers = new HttpHeaders({
-        Authorization: `Bearer ${token}`});
+        Authorization: `Bearer ${token}`,
+        'Access-Control-Allow-Origin': '*'
+      });
         return this.http.get<any>(`${this.baseUrl}/post/{post}/{id}/comment/`, {headers})
   }
 
   public getPost(id: String) {
     const token = this.AuthGuardService.getToken();
     const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`});
+      Authorization: `Bearer ${token}`,
+      'Access-Control-Allow-Origin': '*'
+    });
       return this.http.get<any>(`${this.baseUrl}/post/{post}/{id}/comment/${id}`, {headers});
   }
 }
